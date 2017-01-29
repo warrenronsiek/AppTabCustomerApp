564f13da603d2f9c41eeae618be994a1d623c439
warren <warrenronsiek@gmail.com>
2017-01-29 13:55:55 -0800

    styled the registration scene. now slightly less shit

--- app/redux/components/register.js
+++ app/redux/components/register.js
@@ -2,7 +2,11 @@
  * Created by warren on 1/22/17.
  */
 import React, {PropTypes} from 'react';
-import {View, Button, Text, TextInput, StyleSheet} from 'react-native';
+import {View, Button, Text, TextInput, StyleSheet, Dimensions} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import EnytpoIcon from 'react-native-vector-icons/Entypo'
+import EvilIcon from 'react-native-vector-icons/EvilIcons'
+const {height, width} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -12,43 +16,92 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 40,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  welcomeContainer: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width * .6,
+    flexDirection: 'row'
+  },
+  textInputContainer: {
+    flex: 4,
+    flexDirection: 'column'
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  iconSubContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    flex: 1
   },
   textInputBox: {
-    borderColor: 'gray',
-    borderWidth: 1,
+    flex: 1,
     height: 40
   }
 });
 
 const register = ({updateName, updateEmail, updatePassword, updateConfirmPassword, name, email, password, confirmPassword, registerUser, networkError, userExistsError, unknownError, registering}) => (
   <View style={styles.container}>
-    <Text style={styles.instructions}>
-      Please enter your registration information:
-    </Text>
-    <TextInput style={styles.textInputBox} onChangeText={text => updateName(text)} autoCapitalize='words'
-               autoCorrect={false} value={name} defaultValue="your name"/>
-    <TextInput style={styles.textInputBox} onChangeText={text => updateEmail(text)} autoCapitalize='none'
-               autoCorrect={false} value={email} defaultValue="email"/>
-    <TextInput style={styles.textInputBox} onChangeText={text => updatePassword(text)} autoCapitalize='none'
-               autoCorrect={false} value={password} defaultValue="password"/>
-    <TextInput style={styles.textInputBox} onChangeText={text => updateConfirmPassword(text)} autoCapitalize='none'
-               autoCorrect={false} value={confirmPassword} defaultValue="confirm password"/>
-    <Button onPress={() => registerUser(name, email, password)} title="Register"/>
-    {password === confirmPassword
-      ? <Text>Passwords Match!</Text>
-      : <Text>Passwords dont match!</Text>}
-    {networkError ? <Text>Networking Error!</Text> : null}
-    {unknownError ? <Text>Unknown Error!</Text> : null}
-    {userExistsError ? <Text>User Exists Error!</Text> : null}
-    {registering ? <Text>Registering you now...</Text> : null}
+    <View style={styles.welcomeContainer}>
+      <Text style={styles.welcome}>
+        AppTab
+      </Text>
+      <Text>
+        Alpha 0.1.0
+      </Text>
+    </View>
+    <View style={styles.textContainer}>
+      <View style={styles.iconContainer}>
+        <View style={styles.iconSubContainer}>
+          <EvilIcon name="user" size={30}/>
+        </View>
+        <View style={styles.iconSubContainer}>
+          <MaterialIcon name="email" size={30}/>
+        </View>
+        <View style={styles.iconSubContainer}>
+          <EnytpoIcon name="key" size={30}/>
+        </View>
+        <View style={styles.iconSubContainer}>
+          <EnytpoIcon name="key" size={30}/>
+        </View>
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput style={styles.textInputBox} onChangeText={text => updateName(text)} autoCapitalize='words'
+                   autoCorrect={false} value={name} defaultValue="your name"/>
+        <TextInput style={styles.textInputBox} onChangeText={text => updateEmail(text)} autoCapitalize='none'
+                   autoCorrect={false} value={email} defaultValue="email"/>
+        <TextInput style={styles.textInputBox} onChangeText={text => updatePassword(text)} autoCapitalize='none'
+                   autoCorrect={false} value={password} defaultValue="password"/>
+        <TextInput style={styles.textInputBox} onChangeText={text => updateConfirmPassword(text)} autoCapitalize='none'
+                   autoCorrect={false} value={confirmPassword} defaultValue="confirm password"/>
+      </View>
+    </View>
+    <View style={styles.buttonContainer}>
+      <Button onPress={() => registerUser(name, email, password)} title="Register"/>
+      {password === confirmPassword
+        ? <Text>Passwords Match!</Text>
+        : <Text>Passwords dont match!</Text>}
+      {networkError ? <Text>Networking Error!</Text> : null}
+      {unknownError ? <Text>Unknown Error!</Text> : null}
+      {userExistsError ? <Text>User Exists Error!</Text> : null}
+      {registering ? <Text>Registering you now...</Text> : null}
+    </View>
   </View>
 );
 
