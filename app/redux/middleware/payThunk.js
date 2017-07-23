f8207f2492fb96968c24a9bbe09f9c75555809c2
Warren <warrenronsiek@gmail.com>
2017-07-23 16:44:39 -0700

    fixed ATUA-76, 62

--- app/redux/middleware/payThunk.js
+++ app/redux/middleware/payThunk.js
@@ -7,23 +7,28 @@ import {clearCart} from '../actions/cartActions'
 import * as _ from 'lodash'
 import {Actions, ActionConst} from 'react-native-router-flux'
 
+function round(value, decimals) {
+  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
+
 const payThunk = () => (dispatch, getState) => {
   const state = getState();
   const
     venueId = _.find(state.nodes, ['nodeId', state.activeNode]).venueId,
     currentCart = state.cart.filter(item => item.venueId === venueId);
   const
-    cartTotal = Math.round(_.sum(currentCart.map(item => parseFloat(item.price) * item.count)) * 100),
-    tip = Math.round(cartTotal * state.additionalCosts.tip * 100),
-    tax = Math.round(cartTotal * state.additionalCosts.tax * 100),
-    total = cartTotal + tip + tax,
+    itemTotal = round(_.sum(currentCart.map(item => parseFloat(item.price) * item.count)), 2),
+    tip = round(itemTotal * state.additionalCosts.tip, 2),
+    tax = round(itemTotal * state.additionalCosts.tax, 2),
+    amount = (itemTotal + tip + tax) * 100,
     stripeToken = state.stripeToken,
     cardToken = state.ccTokens.filter(item => item.isSelected)[0].ccToken,
     nodeId = state.activeNode,
     customerId = state.auth.customerId;
 
   return Promise.resolve(dispatch(ccActions.payment.processing()))
-    .then(res => stripeChargeCard({total, stripeToken, cardToken, nodeId, customerId, items: currentCart}))
+    .then(res => stripeChargeCard({amount, stripeToken, cardToken, nodeId, customerId, items: currentCart, tip, tax, itemTotal}))
     .then(res => dispatch(ccActions.payment.success()))
     .then(res => dispatch(clearCart()))
     .then(res => Actions.tabs({type: 'reset'}))
