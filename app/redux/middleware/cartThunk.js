4490e7388fdb4e092ce7cacfe936593d64dbccd9
Warren <warrenronsiek@gmail.com>
2017-09-26 12:15:15 -0700

    now handles case where itemOptions is Null, fixed mistaken copy/paste into vars.js

--- app/redux/middleware/cartThunk.js
+++ app/redux/middleware/cartThunk.js
@@ -8,7 +8,7 @@ const _ = require('lodash');
 
 const addToCartThunk = (itemId) => (dispatch, getState) => {
   const item = _.find(_.flatten(getState().menu.map(section => section.data)), ['itemId', itemId]);
-  if (item.itemOptions) {
+  if (item.itemOptions && (item.itemOptions !== 'NULL')) {
     dispatch(setActiveItem(item.itemName, item.itemDescription, item.price, item.tags, item.category, item.itemId, item.venueId, item.itemOptions));
     Actions.optionsModal()
   } else {
--- app/vars.js
+++ app/vars.js
@@ -9,10 +9,10 @@ if (__DEV__) {
   firehoseName = 'dev-apptab-data-stream';
   identityPoolId = 'us-west-2:f92d5c52-4665-4d22-a58a-2a1937220937';
 } else {
-  url = 'https://zapkwgntzh.execute-api.us-west-2.amazonaws.com/dev';
-  stripePublicKey = 'pk_test_UHQyJcNcx8C65lXY8h9xZwSN'; //actual key: pk_live_FHmywJaXessS64PZR8tK5oOG
-  firehoseName = 'dev-apptab-data-stream';
-  identityPoolId = 'us-west-2:f92d5c52-4665-4d22-a58a-2a1937220937';
+  url = 'https://3qqlpbfcv1.execute-api.us-west-2.amazonaws.com/prod';
+  stripePublicKey = 'pk_live_FHmywJaXessS64PZR8tK5oOG';
+  firehoseName = 'prod-apptab-data-stream';
+  identityPoolId = 'us-west-2:2894af0f-10cd-4d69-8d3a-312ad988c289';
 }
 
 export {url, stripePublicKey, firehoseName, identityPoolId}
