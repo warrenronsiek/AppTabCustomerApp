86d43bfe05e68ec18f2d134b1c71105f36386092
Warren Hamilton Ronsiek <warrenronsiek@gmail.com>
2017-12-10 16:22:19 -0800

    bluetooth warnings no work as intended closing ATUA-188

--- app/common/bleScannerComponentFunctions.js
+++ app/common/bleScannerComponentFunctions.js
@@ -18,72 +18,73 @@ const componentWillMount = () => {
 };
 
 const componentDidMount = () => {
+  BluetoothStatus.state().then(res => {
+    alert('Your phone\'s bluetooth is turned off.\nThe app won\'t be able to detect your restaurant until it is turned on.');
+  });
   const waiter = function () {
-    BluetoothStatus.state()
-      .then(res => {
-        if (!res) {
-          alert('Your phone\'s bluetooth is turned off.\nThe app won\'t be able to detect your restaurant until it is turned on.');
-          throw new Error('Bluetooth not turned on.')
-        } else if (noble.state === 'poweredOn') {
-          noble.startScanning([], true);
-        } else {
-          throw new Error('Noble not powered on.')
-        }
-      })
-      .catch(err => setTimeout(waiter, 100));
+    if (noble.state === 'poweredOn') {
+      noble.startScanning([], true);
+    } else {
+      setTimeout(waiter, 100)
+    }
   };
   waiter()
 };
+
 
 const componentWillUnmount = () => {
   noble.stopScanning();
 };
 
 const onFound = (item) => {
-    parseBlePacket(item)
-      .then(res => {
-        if (res.namespace === 'bc635921402893714ad5') {
-          return Promise.resolve(store.dispatch(updateNodeBle(res.distance, res.namespace, res.instance, res.lastSeen)))
-        }
-        else {
-          throw new WrongNamespaceError('node has incorrect namespace')
-        }
-      })
-      .then(res => {
-        const nodeState = _.find(store.getState().nodes.nodeList, {nodeId: res.nodeId});
-        if (nodeState.apiQueried === undefined) {
-          store.dispatch(setNodeQueried(res.nodeId));
-          return getNodeInfo({nodeId: res.nodeId})
-        } else {
-          throw new GetNodeQueriedError('GetNode api already queried for this node')
-        }
-      })
-      .then(res => {
-        const node = res.nodeInfo.Item;
-        const nodeId = node.NodeId.S, nodeName = node.NodeName.S, nodeDescription = node.NodeDescription.S,
-          venueId = node.VenueId.S;
-        store.dispatch(updateNodeApi(nodeId, nodeName, nodeDescription, venueId));
-        return getVenue({venueId})
-      })
-      .then(res => {
-        store.dispatch(updateVenue({venueId: res.venue.Item.VenueId.S, venueName: res.venue.Item.Name.S, address: res.venue.Item.Address.S}));
-        return Promise.resolve()
-      })
-      .catch(err => {
-        switch (err.name) {
-          case 'TypeError':
-            break;
-          case 'BeaconTypeError':
-            break;
-          case 'GetNodeQueriedError':
-            break;
-          case 'WrongNamespaceError':
-            break;
-          default:
-            logger('error processing node', err);
-            break;
-        }
-      });
-  };
+  parseBlePacket(item)
+    .then(res => {
+      if (res.namespace === 'bc635921402893714ad5') {
+        return Promise.resolve(store.dispatch(updateNodeBle(res.distance, res.namespace, res.instance, res.lastSeen)))
+      }
+      else {
+        throw new WrongNamespaceError('node has incorrect namespace')
+      }
+    })
+    .then(res => {
+      const nodeState = _.find(store.getState().nodes.nodeList, {nodeId: res.nodeId});
+      if (nodeState.apiQueried === undefined) {
+        store.dispatch(setNodeQueried(res.nodeId));
+        return getNodeInfo({nodeId: res.nodeId})
+      } else {
+        throw new GetNodeQueriedError('GetNode api already queried for this node')
+      }
+    })
+    .then(res => {
+      const node = res.nodeInfo.Item;
+      const nodeId = node.NodeId.S, nodeName = node.NodeName.S, nodeDescription = node.NodeDescription.S,
+        venueId = node.VenueId.S;
+      store.dispatch(updateNodeApi(nodeId, nodeName, nodeDescription, venueId));
+      return getVenue({venueId})
+    })
+    .then(res => {
+      store.dispatch(updateVenue({
+        venueId: res.venue.Item.VenueId.S,
+        venueName: res.venue.Item.Name.S,
+        address: res.venue.Item.Address.S
+      }));
+      return Promise.resolve()
+    })
+    .catch(err => {
+      switch (err.name) {
+        case 'TypeError':
+          break;
+        case 'BeaconTypeError':
+          break;
+        case 'GetNodeQueriedError':
+          break;
+        case 'WrongNamespaceError':
+          break;
+        default:
+          logger('error processing node', err);
+          break;
+      }
+    });
+};
 
 export {componentDidMount, componentWillMount, componentWillUnmount}
