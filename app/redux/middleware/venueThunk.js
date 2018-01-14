a9d64147af714047f07d5c65f9efdbc8c67fe24c
Warren Hamilton Ronsiek <warrenronsiek@gmail.com>
2018-01-14 11:03:37 -0800

    pretty sure button fixed for realz this time. Also updated updateMenuItem to dispatch a payload

--- app/redux/middleware/venueThunk.js
+++ app/redux/middleware/venueThunk.js
@@ -4,12 +4,12 @@ import {writeToFirehose, s3} from "../../api/aws";
 import getMenu from "../../api/getMenu";
 import getVenue from "../../api/getVenue";
 import {menuApiQueryStatus, updateMenuItem, updateMenuRanges, updateMenuVisibility} from "../actions/menuActions";
-import {get} from 'lodash'
+import {get, reduce} from 'lodash'
 import {imageBucket} from '../../vars'
 import logger from '../../api/loggingApi'
 import {stopScanning} from "../../common/bleScannerComponentFunctions";
 
-const keyAccessor = item => get(item, 'Image.M.ImageUrl.S', '').split('/').slice(3).reduce((accum, str) => accum + '/' + str, '');
+const keyAccessor = item => reduce(get(item, 'Image.M.ImageUrl.S', '').split('/').slice(3), (accum, str) => accum + '/' + str);
 
 const setActiveVenueThunk = ({venueId, address, venueName}) => (dispatch, getState) => {
   dispatch(updateActiveVenue({venueId, address, venueName}));
@@ -19,26 +19,26 @@ const setActiveVenueThunk = ({venueId, address, venueName}) => (dispatch, getSta
   Promise.all([getMenu({venueId: state.activeVenue.venueId}), getVenue({venueId: state.activeVenue.venueId})])
     .then(res => {
       res[0].Items.forEach(item => {
-        dispatch(updateMenuItem(
-          item.ItemName.S,
-          item.ItemDescription.S,
-          item.Price.N,
-          (JSON.stringify(item.Tags.SS) === JSON.stringify(['NULL'])) ? [] : item.Tags.SS,
-          item.Category.S,
-          item.ItemId.S,
-          item.VenueId.S,
-          (get(item, 'ItemOptions.S', 'NULL') !== 'NULL') ? get(item, 'ItemOptions.S') : '[]',
-          item.TimeRanges.SS,
-          get(item, 'ExtendedDescription.S', ''),
-          !!keyAccessor(item) ? s3.getSignedUrl('getObject', {
+        let key = keyAccessor(item);
+        dispatch(updateMenuItem({
+          itemName:item.ItemName.S,
+            itemDescription: item.ItemDescription.S,
+          price: item.Price.N,
+          tags: (JSON.stringify(item.Tags.SS) === JSON.stringify(['NULL'])) ? [] : item.Tags.SS,
+          category: item.Category.S,
+          itemId: item.ItemId.S,
+          venueId: item.VenueId.S,
+          itemOptions: (get(item, 'ItemOptions.S', 'NULL') !== 'NULL') ? get(item, 'ItemOptions.S') : '[]',
+          timeRanges: item.TimeRanges.SS,
+          extendedDescription: get(item, 'ExtendedDescription.S', ''),
+          imageUrl: !!key ? s3.getSignedUrl('getObject', {
             Bucket: imageBucket,
-            Key: keyAccessor(item)
-          }) : null))
+            Key: key
+          }) : null}))
       });
       res[1].venue.Item.TimeRanges.L.forEach(timeRange =>
         dispatch(updateMenuRanges(timeRange.M.id.S, timeRange.M.range.S))
       );
-      console.log('got to Dispatches')
       dispatch(updateMenuVisibility());
       dispatch(menuApiQueryStatus(venueId, Date.now()));
     })
--- app/vars.js
+++ app/vars.js
@@ -4,16 +4,14 @@
 
 let url, stripePublicKey, firehoseName, identityPoolId, identityPoolName, apiKey, useDevData, imageBucket;
 if (__DEV__) {
-  // url = 'https://ouxlbdc3ja.execute-api.us-west-2.amazonaws.com/dev';
-  url = 'https://3qqlpbfcv1.execute-api.us-west-2.amazonaws.com/prod';
+  url = 'https://ouxlbdc3ja.execute-api.us-west-2.amazonaws.com/dev';
   stripePublicKey = 'pk_test_UHQyJcNcx8C65lXY8h9xZwSN';
   firehoseName = 'dev-apptab-data-stream';
   identityPoolId = 'us-west-2:03e3c8df-0ef1-4b83-a0e2-a4fc652a2815';
   identityPoolName = 'cognito-idp.us-west-2.amazonaws.com/us-west-2_hAAdk6UCT';
-  // apiKey = 'ZACNEBHeBi6ldZZYb6Dzq5QtM8ADM4Qy2LgcpUO6';
-  apiKey = 'VnExuBdEnGav2wK8Hgi1e9jOF4V4H0409CTWzctP';
+  apiKey = 'ZACNEBHeBi6ldZZYb6Dzq5QtM8ADM4Qy2LgcpUO6';
   imageBucket = 'dev-apptab-image-bucket';
-  useDevData = false;
+  useDevData = true;
 } else {
   url = 'https://3qqlpbfcv1.execute-api.us-west-2.amazonaws.com/prod';
   stripePublicKey = 'pk_live_FHmywJaXessS64PZR8tK5oOG';
